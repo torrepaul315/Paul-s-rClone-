3bee7851d58c7610326f50e8f9856958ccaed080
Paul Torres <torrepaul@gmail.com>
2017-05-15 15:31:12 -0600

    got rid of app folder

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 
 var environment = process.env.NODE_ENV || 'development';
-var config = require('knexfile.js')[environment];
+var config = require('./knexfile.js')[environment];
 module.exports = require('knex')(config);
 //  require('knex')(require('./knexfile.js')[process.env.NODE_ENV || 'development'])
