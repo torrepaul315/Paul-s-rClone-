b1d7a9873711e4384f5c37ade377c8e34bc85663
Paul Torres <torrepaul@gmail.com>
2017-05-15 15:00:09 -0600

    knex tweak

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
+require('dotenv').config()
+
 
 if (process.env.NODE_ENV !== 'test') {
   const logger = require('morgan')
