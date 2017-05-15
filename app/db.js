175895499ecbe9d1a80f810b40fa40df337be744
Paul Torres <torrepaul@gmail.com>
2017-05-15 14:11:11 -0600

    knex tweak

--- app/db.js
+++ app/db.js
@@ -1,1 +1,5 @@
-module.exports = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'development'])
+
+var environment = process.env.NODE_ENV || 'development';
+var config = require('./knexfile.js')[environment];
+module.exports = require('knex')(config);
+//  require('knex')(require('./knexfile.js')[process.env.NODE_ENV || 'development'])
